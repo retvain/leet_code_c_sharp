df664081b32b07993aa91587d80a79c9ab004c01
Mikhail Pelagein <retvain@gmail.com>
2022-02-23 15:53:36 +0500

    init

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Привет, мир!!!");
+        }
+    }
+}
